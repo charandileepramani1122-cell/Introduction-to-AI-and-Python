326a04a76c9dcf9c76d31a55a07d84750aff070e
Ramani Charan Dileep <charandileepramani1122@gmail.com>
2026-08-19 10:47:22 +0530

    Create lists.py

---
+++ AI-ML-DL/Python-Basics/lists.py
@@ -0,0 +1,9 @@
+# Python Lists
+
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+
+print("Fruits:", fruits)
+print("First fruit:", fruits[0])
+
+fruits.append("Grapes")
+print("Updated list:", fruits)
